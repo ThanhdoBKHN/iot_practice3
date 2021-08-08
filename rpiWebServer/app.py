202cbb28b5af595768198e46e3523e24934a892c
Thanh Do <dothanhqt9x@gmail.com>
2021-08-08 15:28:55 +0700

    bai thuc hanh nhom 21

--- rpiWebServer/app.py
+++ rpiWebServer/app.py
@@ -17,28 +17,45 @@ ledGreenSts = 0
 #GPIO.output(ledGreen, GPIO.LOW) 
 @app.route('/') 
 def index(): 
-    ledRedSts = ledRed 
-    ledYellowSts = ledYellow
-    ledGreenSts = ledGreen
+    ledRedSts = str(ledRed) + " OFF" 
+    ledYellowSts = str(ledYellow) + " OFF" 
+    ledGreenSts = str(ledGreen) + " OFF" 
     templateData = { 'ledRed' : ledRedSts, 'ledYellow' : ledYellowSts, 'ledGreen' : ledGreenSts } 
     return render_template('index.html', **templateData)
 @app.route('/<deviceName>/<action>') 
 def do(deviceName, action): 
-    if deviceName == "ledRed": 
+    ledRedS = " OFF"
+    ledYellowS = " OFF"
+    ledGreenS = " OFF"
+
+    if deviceName == "ledRed" and action == "on": 
         actuator = ledRed 
-    if deviceName == "ledYellow": 
+        ledRedS = " ON"
+    if deviceName == "ledYellow" and action == "on": 
         actuator = ledYellow 
-    if deviceName == "ledGreen": 
+        ledYellowS = " ON"
+    if deviceName == "ledGreen" and action == "on": 
         actuator = ledGreen 
+        ledGreenS = " ON"
+    if deviceName == "ledRed" and action == "off": 
+        actuator = ledRed 
+        ledRedS = " OFF"
+    if deviceName == "ledYellow" and action == "off": 
+        actuator = ledYellow 
+        ledYellowS = " OFF"
+    if deviceName == "ledGreen" and action == "off": 
+        actuator = ledGreen 
+        ledGreenS = " OFF"
+
     if action == "on": 
         actuator = 1
     if action == "off": 
         actuator = 0
 
-    ledRedSts = ledRed
-    ledYellowSts = ledYellow
-    ledGreenSts = ledGreen
-    templateData = { 'ledRed' : ledRedSts, 'ledYellow' : ledYellowSts, 'ledGreen' : ledGreenSts }
+    ledRedSts = str(ledRed) + ledRedS
+    ledYellowSts = str(ledYellow) + ledYellowS
+    ledGreenSts = str(ledGreen) + ledGreenS
+    templateData = { 'ledRed' : ledRedSts, 'ledYellow' : ledYellowSts, 'ledGreen' : ledGreenSts}
     return render_template('index.html', **templateData )
 
 if __name__ == '__main__': 
