743f7e4585756a0499f54a6cd51ef31c92762950
Thanh Do <dothanhqt9x@gmail.com>
2021-08-08 15:05:27 +0700

    the second

--- rpiWebServer/app.py
+++ rpiWebServer/app.py
@@ -1,27 +1,26 @@
-import RPi.GPIO as GPIO 
+# import RPi.GPIO as GPIO 
 from flask import Flask, render_template, request 
 app = Flask(__name__) 
-GPIO.setmode(GPIO.BCM) 
-GPIO.setwarnings(False) 
+#GPIO.setmode(GPIO.BCM) 
+#GPIO.setwarnings(False) 
 ledRed = 13 
 ledYellow= 19 
 ledGreen= 26 
 ledRedSts = 0
 ledYellowSts = 0 
 ledGreenSts = 0 
-GPIO.setup(ledRed, GPIO.OUT) 
-GPIO.setup(ledYellow,GPIO.OUT) 
-GPIO.setup(ledGreen, GPIO.OUT) 
-GPIO.output(ledRed, GPIO.LOW) 
-GPIO.output(ledYellow, GPIO.LOW) 
-GPIO.output(ledGreen, GPIO.LOW) 
+#GPIO.setup(ledRed, GPIO.OUT) 
+#GPIO.setup(ledYellow,GPIO.OUT) 
+#GPIO.setup(ledGreen, GPIO.OUT) 
+#GPIO.output(ledRed, GPIO.LOW) 
+#GPIO.output(ledYellow, GPIO.LOW) 
+#GPIO.output(ledGreen, GPIO.LOW) 
 @app.route('/') 
 def index(): 
-    ledRedSts = GPIO.input(ledRed) 
-    ledYellowSts = GPIO.input(ledYellow) 
-    ledGreenSts = GPIO.input(ledGreen) 
-    templateData = { 'ledRed' : ledRedSts, 'ledYellow' : 
-ledYellowSts, 'ledGreen' : ledGreenSts } 
+    ledRedSts = ledRed 
+    ledYellowSts = ledYellow
+    ledGreenSts = ledGreen
+    templateData = { 'ledRed' : ledRedSts, 'ledYellow' : ledYellowSts, 'ledGreen' : ledGreenSts } 
     return render_template('index.html', **templateData)
 @app.route('/<deviceName>/<action>') 
 def do(deviceName, action): 
@@ -32,13 +31,15 @@ def do(deviceName, action):
     if deviceName == "ledGreen": 
         actuator = ledGreen 
     if action == "on": 
-        GPIO.output(actuator, GPIO.HIGH) 
+        actuator = 1
     if action == "off": 
-        GPIO.output(actuator, GPIO.LOW) 
+        actuator = 0
 
-    ledRedSts = GPIO.input(ledRed) 
-    ledYellowSts = GPIO.input(ledYellow) 
-    ledGreenSts = GPIO.input(ledGreen) 
-    templateData = { 'ledRed' : ledRedSts, 'ledYellow' : ledYellowSts, 
-    'ledGreen' : ledGreenSts }
+    ledRedSts = ledRed
+    ledYellowSts = ledYellow
+    ledGreenSts = ledGreen
+    templateData = { 'ledRed' : ledRedSts, 'ledYellow' : ledYellowSts, 'ledGreen' : ledGreenSts }
     return render_template('index.html', **templateData )
+
+if __name__ == '__main__': 
+    app.run(debug=True,host='0.0.0.0')
